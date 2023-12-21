97e43d87f86c240e5b707e99fe447355ca1c726f
Tola Voeung <tolacambo@gmail.com>
2023-12-21 16:13:00 +0700

    committed files

---
+++ App.js
@@ -0,0 +1,18 @@
+
+
+import { StatusBar } from 'expo-status-bar'; 
+import {  AuthProvider } from './context/AuthContext';
+import AppNav from './components/AppNav';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style='light' />
+
+      <AuthProvider>
+        <AppNav/> 
+      </AuthProvider>
+    </>
+  );
+}
+ 
